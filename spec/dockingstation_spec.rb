54a24f5b6b8051e52b2b2b797d3e0cfff547975d
Tomas Garcia <tomasgarciadev@gmail.com>
2021-11-17 15:01:32 +0000

    require_relative add to dockingstation and release_bike Bike instance created (10)

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,4 +5,13 @@ describe DockingStation do
     docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike)
   end
+
+  it 'should create a new instance of bike' do
+    expect(Bike.new).to be_an_instance_of(Bike)
+  end 
+
+  it 'Bike instance should respond to working?' do
+    bike = Bike.new
+    expect(bike).to respond_to(:working?)
+  end
 end
