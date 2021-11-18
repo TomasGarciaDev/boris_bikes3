59ff24d1af9915b8176adfc2bcb6160fed39b98a
Tomas Garcia <tomasgarciadev@gmail.com>
2021-11-18 15:50:06 +0000

    update capacity to 20 (step 14)

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -12,21 +12,33 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
   
 
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
   
 
   it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    station = DockingStation.new
+    first_state = station.bikes.length
+    station.dock(Bike.new)
+    expect(station.bikes.length).to eq first_state + 1
   end
  
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes).to eq Array.new.push(bike)
   end
 
   it "raise an error when there are no bikes" do
     expect {subject.release_bike}.to raise_error("No bikes available")
+  end
+
+  # it "raise an error when docking a bit if there is already a bike" do
+  #   subject.dock(Bike.new)
+  #   expect {subject.dock(Bike.new)}.to raise_error("Station full")
+  # end
+
+  it "raised an error if there are over 20 bikes" do
+    20.times { subject.dock Bike.new }
+    expect { subject.dock Bike.new }.to raise_error("Station full")
   end
 end
