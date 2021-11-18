b69f55e75be3fd9cf074d522ebf30d3514944c9f
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-11-18 16:10:26 +0000

    Default capacity constant added

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -38,7 +38,7 @@ describe DockingStation do
   # end
 
   it "raised an error if there are over 20 bikes" do
-    20.times { subject.dock Bike.new }
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
     expect { subject.dock Bike.new }.to raise_error("Station full")
   end
 end
